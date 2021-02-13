8f6e2e29abd1d8eaca3f8c71e0ad2df716acaa00
ilyes <ilyes.huus@gmail.com>
2021-02-13 18:19:41 +0100

    commit chapterOne

--- src/Component/ChapterOne.js
+++ src/Component/ChapterOne.js
@@ -4,6 +4,15 @@ function ChapterOne() {
   return (
     <div>
       <h1>First CHapter : The Past</h1>
+
+      <input />
+      <image />
+      <div>
+        <p>hello left</p>
+      </div>
+      <div>
+        <p>hello right</p>
+      </div>
     </div>
   );
 }
