2b80bd2f6cd496791f77926845fd222b8843bf46
ilyes <ilyes.huus@gmail.com>
2021-02-13 17:32:41 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import ChapterOne from './Component/ChapterOne';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/chapterOne' component={ChapterOne}></Route>
+          <Route exact path='/chapterTwo'></Route>
+          <Route exact path='/chapterThree'></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Component/ChapterOne.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChapterOne() {
+  return (
+    <div>
+      <h1>First CHapter : The Past</h1>
+    </div>
+  );
+}
+
+export default ChapterOne;
