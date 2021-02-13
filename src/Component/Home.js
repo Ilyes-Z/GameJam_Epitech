1612dcb4787a9c71e28e0a96c389c7d9aef23bb5
ilyes <ilyes.huus@gmail.com>
2021-02-13 21:16:27 +0100

    home redirection to chapter

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,8 +1,31 @@
-import react from 'react';
+import React from 'react';
+import { AppBar, Box, Button, IconButton, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles(theme => ({
+  link: {
+    color: '#000000',
+  }
+}));
 
 const Home = () => {
+  const classes = useStyles();
   return (
-    <h1>HOME</h1>
+    <div>
+      <h1>HOME</h1>
+      <div>
+        <Link to='/chapterOne' className={classes.link}><h3>Chapter One: The Past</h3></Link>
+        <p>zbeuuuul</p>
+      </div>
+      <div>
+        <Link to='/chapterTwo' className={classes.link}><h3>Chapter Two: The Present</h3></Link>
+        <p>WAAAAAARGH</p>
+      </div>
+      <div>
+        <Link to='/chapterThree' className={classes.link}><h3>Chapter Three: The Futur</h3></Link>
+        <p>ARARARARRARAR</p>
+      </div>
+    </div>
   );
 }
 
